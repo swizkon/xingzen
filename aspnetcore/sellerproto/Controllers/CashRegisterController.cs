84aee18ca806aaacff870d82718593f3d1adf5f3
swizkon <jonas@jerndin.se>
2018-06-20 00:49:49 +0200

    Login with AAD B2C

--- aspnetcore/sellerproto/Controllers/CashRegisterController.cs
+++ aspnetcore/sellerproto/Controllers/CashRegisterController.cs
@@ -3,26 +3,30 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using sellerproto.Models;
 
 namespace sellerproto.Controllers
 {
-    public class StoreController : Controller
+    public class CashRegisterController : Controller
     {
+        [Authorize]
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
+        [Authorize]
+        public IActionResult Display()
         {
             ViewData["Message"] = "Your application description page.";
             ViewData["Store"] = base.HttpContext.Request.Query.FirstOrDefault(x => x.Key == "store").Value;
             return View();
         }
 
-        public IActionResult Contact()
+        [Authorize]
+        public IActionResult Numpad()
         {
             ViewData["Message"] = "Your contact page.";
             ViewData["Store"] = base.HttpContext.Request.Query.FirstOrDefault(x => x.Key == "store").Value;
--- aspnetcore/sellerproto/Startup.cs
+++ aspnetcore/sellerproto/Startup.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Hubs;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using sellerproto.Infrastructure;
 
 namespace sellerproto
 {
@@ -31,9 +34,15 @@ namespace sellerproto
             {
                 app.UseExceptionHandler("/Home/Error");
             }
-            
-            var builder = new ConfigurationBuilder();
-            
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+
+            /*
             if (env.IsDevelopment())
             {
                 builder.SetBasePath(env.ContentRootPath)
@@ -42,32 +51,48 @@ namespace sellerproto
                         .AddJsonFile("appsettings.Development.json", optional: true)
                         .AddEnvironmentVariables();
             }
-
             Configuration = builder.Build();
+            */
 
-            app.UseStaticFiles();
-
-                app.UseSignalR(routes =>
+            app.UseStaticFiles()
+                .UseSession()
+                .UseAuthentication()
+                .UseSignalR(routes =>
                 {
                     routes.MapHub<TransactionHub>("/transactionHub");
-                });
-
-            app.UseMvc(routes =>
+                })
+                .UseMvc(routes =>
                 {
                     routes.MapRoute(
                         name: "default",
                         template: "{controller=Home}/{action=Index}/{id?}");
                 });
         }
-        
-        
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             Console.WriteLine("ConfigureServices(IServiceCollection services)");
+
+            services.AddAuthentication(sharedOptions =>
+            {
+                sharedOptions.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                sharedOptions.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            })
+            .AddAzureAdB2C(options => Configuration.Bind("Authentication:AzureAdB2C", options))
+            .AddCookie();
+
             services.AddMvc();
             services.AddSignalR();
-            
+
+            services.AddDistributedMemoryCache();
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromHours(1);
+                options.Cookie.HttpOnly = true;
+            });
+
             services.AddLogging();
         }
     }
